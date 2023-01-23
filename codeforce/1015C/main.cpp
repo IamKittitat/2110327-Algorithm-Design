7b1bb72e7e10e43e81ff5f716b6895ea2ad561e4
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-01-23 10:38:10 +0700

    codeforce_1015C

---
+++ codeforce/1015C/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    /*int n,m;
+    cin >> n >> m;
+    vector<double> v(n);
+    double total = 0;
+    for(int i = 0; i < n;i++){
+        int bef,af;
+        cin >> bef >> af;
+        v[i] = bef-af;
+        total += bef;
+    }
+    int ans = 0;
+    sort(v.begin(),v.end());
+    for(int i = n-1;i>=0;i--){
+        if(total <= m) break;
+        else{
+            total -= v[i];
+            ans++;
+        }
+    }
+    if(total <= m) cout << ans;
+    else cout << "-1";*/
+    cout << LLONG_MAX;
+}
