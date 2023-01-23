e7af10869a5e4ae5527c9e7c3c201ec0c2fdf03e
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-01-23 10:43:00 +0700

    984A

---
+++ codeforce/984A/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int i = 0;i<n;i++) cin >> v[i];
+    sort(v.begin(),v.end());
+    if(v.size()%2 == 0){
+        cout << v[v.size()/2-1];
+    } else{
+        cout << v[v.size()/2];
+    }
+}
