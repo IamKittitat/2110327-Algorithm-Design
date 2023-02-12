af5df00c1d55bfde3c5c68b5433ae29b74a090a8
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-02-12 13:02:12 +0700

    revised permutaion & combination

--- youtube/combination/main.cpp
+++ youtube/combination/main.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
 
 using namespace std;
 
@@ -8,16 +9,15 @@ void combi(int n,vector<int> &sol,int len){
         combi(n,sol,len+1);
         sol[len] = 1;
         combi(n,sol,len+1);
-
     } else{
-        for(int i = 0;i<n;i++){
-            if(sol[i] == 1) cout << i+1 << " ";
+        for(int i =0;i<sol.size();i++){
+            if(sol[i]) cout << i+1 << " ";
         }
-        cout << "." << endl;
+        cout <<"." <<endl;
     }
 }
 
-void combi_kn(int n,vector<int> &sol,int len,int k, int chosen){
+void combi_kn(int n,vector<int> &sol,int len,int k,int chosen){
     if(len < n){
         sol[len] = 0;
         combi_kn(n,sol,len+1,k,chosen);
@@ -26,37 +26,40 @@ void combi_kn(int n,vector<int> &sol,int len,int k, int chosen){
             combi_kn(n,sol,len+1,k,chosen+1);
         }
     } else{
-        for(int i = 0;i<n;i++){
+        for(int i =0;i<n;i++){
             if(sol[i] == 1) cout << i+1 << " ";
         }
-        cout << "." << endl;
+        cout << ".\n";
     }
 }
 
-void combi_exact(int n,vector<int> &sol,int len,int k, int chosen){
+void combi_exact(int n,vector<int> &sol,int len,int k,int chosen){
     if(len < n){
-        if(len-chosen < n-k){
-            sol[len] = 0;
-            combi_exact(n,sol,len+1,k,chosen);
-        }
-
+        // Can choose
         if(chosen < k){
             sol[len] = 1;
             combi_exact(n,sol,len+1,k,chosen+1);
         }
+        // Can not choose
+        if(len - chosen < n - k){
+            sol[len] = 0;
+            combi_exact(n,sol,len+1,k,chosen);
+        }
     } else{
-        for(int i = 0;i<n;i++){
+        for(int i =0;i<n;i++){
             if(sol[i] == 1) cout << i+1 << " ";
         }
-        cout << "." << endl;
+        cout << ".\n";
     }
 }
 
+
 int main()
 {
-    int n = 4;
+    int n,k;
+    cin >> n >> k;
     vector<int> sol(n);
-    combi_exact(n,sol,0,2,0);
-    // combi_kn(n,sol,0,2,0);
     // combi(n,sol,0);
+    // combi_kn(n,sol,0,k,0);
+    combi_exact(n,sol,0,k,0);
 }
--- youtube/permutation/main.cpp
+++ youtube/permutation/main.cpp
@@ -1,27 +1,29 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include<vector>
 
 using namespace std;
 
-void perm(int n, vector<int> &sol,int len, vector<bool> &used){
+void perm(int n,vector<int> &sol,vector<bool> &used,int len){
     if(len < n){
-        for(int i = 1 ;i <=n ;i++){
-            if(used[i] == false){
+        for(int i = 1;i<=n;i++){
+            if(!used[i]){
                 used[i] = true;
                 sol[len] = i;
-                perm(n,sol,len+1,used);
+                perm(n,sol,used,len+1);
                 used[i] = false;
             }
         }
     } else{
-        for(auto &x : sol) cout << x;
-        cout << endl;
+        for(int i =0;i<sol.size();i++) cout << sol[i] << " ";
+        cout << "." << endl;
     }
+    
 }
 
-void perm_kn(int n, vector<int> &sol,int len, vector<bool> &used,int k){
+void perm_kn(int n,vector<int> &sol,int len,vector<bool> &used,int k){
     if(len < k){
-        for(int i = 1 ;i <=n ;i++){
-            if(used[i] == false){
+        for(int i =1;i<=n;i++){
+            if(!used[i]){
                 used[i] = true;
                 sol[len] = i;
                 perm_kn(n,sol,len+1,used,k);
@@ -29,29 +31,31 @@ void perm_kn(int n, vector<int> &sol,int len, vector<bool> &used,int k){
             }
         }
     } else{
-        for(auto &x : sol) cout << x;
+        for(int i =0;i<k;i++) cout << sol[i] << " ";
         cout << endl;
     }
 }
 
-void perm_kn_replace(int n, vector<int> &sol,int len,int k){
+void perm_kn_replace(int n,vector<int> &sol,int len,int k){
     if(len < k){
-        for(int i = 1 ;i <=n ;i++){
+        for(int i =1;i<=n;i++){
             sol[len] = i;
             perm_kn_replace(n,sol,len+1,k);
         }
-    } else{
-        for(auto &x : sol) cout << x;
+     }
+    else{
+        for(int i =0;i<k;i++) cout << sol[i];
         cout << endl;
     }
 }
 
-
 int main()
 {
-    int n = 3;
+    int n,k;
+    cin >> n >> k;
     vector<int> sol(n);
     vector<bool> used(n);
-    perm_kn_replace(4,sol,0,3);
-    // perm(n,sol,0,used);
+    // perm(n,sol,used,0);
+    // perm_kn(n,sol,0,used,k);
+    perm_kn_replace(n,sol,0,k);
 }
