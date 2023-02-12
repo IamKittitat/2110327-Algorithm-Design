2d3a472c4988cd74b35d1e69745067653b8956bf
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-02-12 09:42:41 +0700

    a59_midp3_similar

--- grader/a59_midp3_similar/main.cpp
+++ grader/a59_midp3_similar/main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-bool solve(string s1,string s2,int l1,int r1,int l2,int r2){
+bool solve(string &s1,string &s2,int l1,int r1,int l2,int r2){
     int len1 = r1-l1+1;
     int len2 = r2-l2+1;
     if(r1 == l1) return s1.substr(l1,len1) == s2.substr(l2,len2);
@@ -12,8 +12,9 @@ bool solve(string s1,string s2,int l1,int r1,int l2,int r2){
     int m1 = (l1+r1)/2;
     int m2 = (l2+r2)/2;
     bool firstCon = solve(s1,s2,l1,m1,l2,m2) && solve(s1,s2,m1+1,r1,m2+1,r2);
-    if(firstCon) return true;
+   
     bool secondCon = solve(s1,s2,l1,m1,m2+1,r2) && solve(s1,s2,m1+1,r1,l2,m2);
+    if(firstCon) return true;
     if(secondCon) return true;
 
     return false;
@@ -21,6 +22,7 @@ bool solve(string s1,string s2,int l1,int r1,int l2,int r2){
 
 
 int main(){
+    ios_base::sync_with_stdio(false); cin.tie(0);
     string s1,s2;
     cin >> s1 >> s2;
     if(solve(s1,s2,0,s1.size()-1,0,s2.size()-1)) cout << "YES";
