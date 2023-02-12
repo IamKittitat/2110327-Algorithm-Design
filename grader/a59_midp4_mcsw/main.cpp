de1120f9d8b4d2586c288cd074b2d426815e93a1
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-02-12 18:28:35 +0700

    a59_midp4_mcsw

--- grader/a59_midp4_mcsw/main.cpp
+++ grader/a59_midp4_mcsw/main.cpp
@@ -6,35 +6,26 @@ using namespace std;
 
 deque<int> deq;
 
-int mss(vector<int> &v,int w,vector<int> &S){
+int solve(vector<int> &v,int w,vector<int> &S){
     int best = INT32_MIN;
-    for(int i = 0;i<S.size();i++){
-        if(!deq.empty() && i - deq.front() > w) deq.pop_front();
+    for(int i =0;i<S.size();i++){
+        if(!deq.empty() && i - deq.front() >= w) deq.pop_front();
 
-        if(i !=  0 && S[i] - S[deq.front()] > best){
-            best = S[i] - S[deq.front()];
-        } 
-        while(!deq.empty() && S[deq.back()] > S[i]) deq.pop_back();
+        if(i != 0 && !deq.empty())best = max(S[i]-S[deq.front()],best);
+
+        while(!deq.empty() && S[deq.back()] >= S[i]) deq.pop_back();
         deq.push_back(i);
+        cout << deq.size() << endl;
     }
     return best;
 }
 
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(0);
     int n,w;
     cin >> n >> w;
     vector<int> v(n),S(n+1);
     for(int i =0;i<n;i++) cin >> v[i];
     S[0] = 0;
-    for(int i = 0; i < n ; i++) S[i+1] = S[i] + v[i];
-    cout << mss(v,w,S);
+    for(int i =0;i<n;i++) S[i+1] = v[i] + S[i];
+    cout << solve(v,w,S);
 }
-
-/*
-5 3
--10 -2 -3 -4 -5
-
-10 4
-1 4 2 -3 5 -7 3 9 2 -7
-*/
