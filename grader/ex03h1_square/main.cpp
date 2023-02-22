18698f5b2809f6e9325fe5ce15c1cba3a2fedab3
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-02-22 20:48:17 +0700

    ex03h1_square

---
+++ grader/ex03h1_square/main.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    int r,c;
+    cin >> r >> c;
+    vector<vector<int>> v(r,vector<int>(c)),dp(r,vector<int>(c));
+    
+    for(int i =0;i<r;i++){
+        string s;
+        cin >> s;
+        for(int j =0;j<c;j++){
+            v[i][j] = s[j] == '1' ? 1 : 0;
+        }
+    }
+
+    // for(auto &x : v){
+    //     for(auto &y : x) cout << y << " ";
+    //     cout << endl;
+    // }
+
+    for(int i =0;i<r;i++) dp[i][0] = (v[i][0] == 1) ? 1 : 0;
+    for(int i =0;i<c;i++) dp[0][i] = (v[0][i] == 1) ? 1 : 0;
+
+    for(int i =1;i<r;i++){
+        for(int j = 1;j<c;j++){
+            if(v[i][j] == 0) continue;
+            dp[i][j] = min(min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]) + 1;
+        }
+    }
+
+    int ans = 0;
+    for(int i =0;i<r;i++) for(int j=0;j<c;j++) ans = max(ans,dp[i][j]);
+    cout << ans;
+}
