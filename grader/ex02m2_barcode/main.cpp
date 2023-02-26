a6efc9fc9616bfa85d55fdde6854b8ee4f39064f
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-02-26 15:32:38 +0700

    ex02m2_barcode

--- grader/ex02m2_barcode/main.cpp
+++ grader/ex02m2_barcode/main.cpp
@@ -3,31 +3,55 @@
 
 using namespace std;
 
-int dp[35][35]; // length i with j swap times
+int main(){
+    int n,m,k;
+    cin >> n >> m >> k;
+    vector<vector<int>> dp(n+1,vector<int>(k+1,0));
+    for(int i =1;i<=n;i++) dp[i][0] = i > m ? 0 : 1;
+    // Loop every row
+    for(int i =1;i<=n;i++){
+        // Loop every col
+        for(int j =1;j<=k;j++){
+            // Sum every i-l (Doesnt break M law)
+            for(int l = 1;l<=m;l++){
+                if(l > i) break;
+                dp[i][j] += dp[i-l][j-1];
+            }
+        }
+    }
+    cout << dp[n][k];
+}
+
+/*
+Top Down Approach]
+
+int dp[35][35];
+
+int solve(int n,int m,int k){
+    if(n<1) return 0;
+    if(k == 0){
+        if(n <= m) return 1;
+        return 0;
+    }
+    if(dp[n][k] > -1) return dp[n][k];
+    int result = 0;
+    for(int i =1;i<=m;i++){
+        result += solve(n-i,m,k-1);
+    }
+    dp[n][k] = result;
+    return result;
+
+}
 
 int main(){
     int n,m,k;
     cin >> n >> m >> k;
-    // Initial Case for col 0 
-    for(int i =0;i<=n;i++){
-        if(i<=m) dp[i][0] = 1;
-        else dp[i][0] = 0;
-    }
-    // Len 0 can create 0 
-    dp[0][0] = 0;
-
-    for(int i =1;i<=n;i++){
-        for(int j=1;j<=k;j++){
-            dp[i][j] = 0;
-            // For "m" (Can have same color within this range)
-            for(int l =1;l<=m;l++){
-                if(l>i) break;
-                dp[i][j] += dp[i-l][j-1];
-            }
-        } 
-    }
-    cout << dp[n][k];
+    for(int i =0;i<=n;i++) for(int j=0;j<=k;j++) dp[i][j] = -1;
+    cout << solve(n,m,k);
 }
+*/
+
+
 /*
 4 3 1
 
