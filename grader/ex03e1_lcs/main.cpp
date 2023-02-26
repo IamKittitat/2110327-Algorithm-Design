e303e2370c9f1cdbfed545249454644ca35da240
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-02-26 18:08:23 +0700

    ex03e1_lcs

--- grader/ex03e1_lcs/main.cpp
+++ grader/ex03e1_lcs/main.cpp
@@ -1,41 +1,33 @@
 #include<iostream>
-#include<string>
+#include<vector>
 
 using namespace std;
 
-int dp[505][505];
-
-int bottomUp(string &s1,string &s2){
-    // s1.size() row , s2.size() col
-    for(int i =0;i<=s1.size();i++) dp[i][0] = 0;
-    for(int i =0;i<=s2.size();i++) dp[0][i] = 0;
-
-    for(int i =1;i<=s1.size();i++){
-        for(int j =1;j<=s2.size();j++){
-            if(s1[i-1] == s2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
-            else{
-                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-    }
-    return dp[s1.size()][s2.size()];
-}
-
-int solve(string &s1,string &s2,int r1,int r2){
-    if(r1 < 0 || r2 < 0) return 0;
-    if(dp[r1][r2] > 0) return dp[r1][r2];
-
-    int result;
-    if(s1[r1] == s2[r2]) result = 1 + solve(s1,s2,r1-1,r2-1);
-    else result = max(solve(s1,s2,r1-1,r2),solve(s1,s2,r1,r2-1));
-
-    dp[r1][r2] = result;
-    return result;
-}
-
 int main(){
+    int n,m;
+    cin >> n >> m;
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
     string s1,s2;
     cin >> s1 >> s2;
-    // cout << solve(s1,s2,s1.size()-1,s2.size()-1);
-    cout << bottomUp(s1,s2);
+    s1 = " " + s1;
+    s2 = " " + s2;
+    for(int i = 1;i<=n;i++){
+        for(int j =1;j<=m;j++){
+            if(s1[i] == s2[j]) dp[i][j] = dp[i-1][j-1] + 1;
+            else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+    
+    string ans = "";
+    int r = n, c = m;
+    while(r>=1 && c >= 1){
+        if(s1[r] == s2[c]){
+            ans = s1[r] + ans;
+            r--; c--;
+        } else{
+            if(dp[r-1][c] > dp[r][c-1]) r--;
+            else c--;
+        }
+    }
+    cout << ans;
 }
