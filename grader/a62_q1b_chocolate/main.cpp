f4708dc77f1e537472806ac6eb345cf05330fbd7
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-02-26 14:32:05 +0700

    a62_q1b_chocolate

--- grader/a62_q1b_chocolate/main.cpp
+++ grader/a62_q1b_chocolate/main.cpp
@@ -10,14 +10,12 @@ int main(){
     vector<int> v(k),dp(n+1);
     for(int i = 0;i<k;i++) cin >> v[i];
     sort(v.begin(),v.end());
-    dp[0] = 0;
     for(int i =1;i<=n;i++){
-        dp[i] = 0;
-        for(int j = 0;j<k;j++){
+        for(int j =0;j<k;j++){
             if(v[j] > i) break;
-            if(v[j] == i) dp[i]++;
+            if(i == v[j]) dp[i]++;
             dp[i] = (dp[i] + dp[i-v[j]])%1000003;
         }
-    }      
+    }
     cout << dp[n];
 }
