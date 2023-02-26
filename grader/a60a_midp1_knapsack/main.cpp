ffa88a381d40f4f408bca2df6229910b49ad8e4d
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-02-26 18:23:43 +0700

    a60a_midp1_knapsack

--- grader/a60a_midp1_knapsack/main.cpp
+++ grader/a60a_midp1_knapsack/main.cpp
@@ -7,34 +7,31 @@ int main(){
     int n,m;
     cin >> n >> m;
     vector<int> v(n+1),w(n+1);
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
     for(int i =1;i<=n;i++) cin >> v[i];
     for(int i =1;i<=n;i++) cin >> w[i];
-    vector<vector<int>> dp(n+1);
-    for(int i=0;i<=n;i++){
-        vector<int> tmp(m+1);
-        for(int j = 0;j<=m;j++) cin >> tmp[j];
-        dp[i] = tmp;
+    for(int i =1;i<=n;i++){
+        for(int j =1;j<=m;j++){
+            if(w[i] > j) dp[i][j] = dp[i-1][j];
+            else{
+                dp[i][j] = max(dp[i-1][j],dp[i-1][j-w[i]] + v[i]);
+            }
+        }
     }
 
     vector<int> ans;
-    // int r = n,c = m;
-    // while(r >0 && c >0){
-    //     if(w[r] <= c){
-    //         if(dp[r][c] == dp[r-1][c-w[r]] + v[r]){
-    //             ans.push_back(r);
-    //             c-=w[r];
-    //         }
-    //     }
-    //     r--;
-    // }
-    int col = m;
-    for(int i = n;i>0;i--){
-        if(dp[i][col] == 0 || col <= 0) break;
-        if(w[i] <= col && dp[i-1][col-w[i]] + v[i] == dp[i][col]){
-            ans.push_back(i);
-            col -= w[i];
+    
+    int r = n,c = m;
+    while(r >= 1 && c >= 1){
+        // cout << r << " " << c << endl;
+        if(w[r] <= c && dp[r][c] - v[r] == dp[r-1][c-w[r]]){
+            ans.push_back(r);
+            c-=w[r];
         } 
+        r--;
     }
     cout << ans.size() << endl;
     for(auto &x : ans) cout << x << " ";
+
+
 }
