c42a936b19699a38c3b34ebeac938e29eb565a85
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-03-09 12:52:09 +0700

    a60_q1_number1

--- grader/a60_q1_number1/main.cpp
+++ grader/a60_q1_number1/main.cpp
@@ -1,35 +1,26 @@
 #include<iostream>
-#include<vector>
 
 using namespace std;
 
-int solve(int n,int start,int stop,int l,int r){
-    // cout << n << " " << start << " " << stop << " " <<  l << " " << r << endl;
+int getLen(int n){
+    if(n == 1) return 1;
+    return 2*getLen(n/2) + 1;
+}
+
+int solve(int n,int l,int r,int start,int stop){
+    if(l > r) return 0;
     if(n == 1) return 1;
     if(n == 0) return 0;
 
     int m = (start+stop)/2;
-    int left =0,right=0;
-    // Divide to [left] mid [right]
-    if(l<m) left = solve(n/2,start,m-1,l,min(m-1,r)); // min(m-1,r) for right pointer in the left case.
-    if(r>m) right = solve(n/2,m+1,stop,max(m+1,l),r); // max(m+1,l) for left pointer in the right case.
-
-    int ans = left + right;
-    // Check mid condition if %2 == 1
-    if(l<=m && r >=m && n%2 == 1) ans++;
-    return ans;
+    int leftAns = solve(n/2,l,min(r,m-1),start,m-1);
+    int midAns = n%2 == 1 && (l <= m && r >= m) ? 1 : 0;
+    int rightAns = solve(n/2,max(m+1,l),r,m+1,stop);
+    return leftAns + midAns + rightAns;
 }
 
 int main(){
     int n,l,r;
     cin >> n >> l >> r;
-
-    int tmp = n,len = 1,i = 2;
-    while(tmp>1){
-        len += i;
-        tmp /= 2;
-        i*=2;
-    }
-
-    cout << solve(n,0,len-1,l-1,r-1);
-}  
+    cout << solve(n,l,r,1,getLen(n));
+}
