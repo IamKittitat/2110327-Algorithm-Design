d5d5c0e95ea45a8265dcb9f80b63293d64b60e41
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-03-09 11:57:27 +0700

    ex01e3

--- grader/ex01e3/main.cpp
+++ grader/ex01e3/main.cpp
@@ -1,28 +1,31 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
 
 using namespace std;
 
-int solve(vector<int> &v,int q,int start,int stop,int ans){
-    if(start == stop){
-        if(v[start] > ans && v[start] <= q) ans = v[start];
+int solve(vector<int> &v, int q,int l,int r,int ans){
+    if(l == r){
+        if(v[l] <= q){
+            return max(ans,v[l]);
+        }
         return ans;
     }
-    int m = (start+stop)>>1;
-    if(v[m] > q) return solve(v,q,start,m,ans);
-    else{
-        return solve(v,q,m+1,stop,v[m]);
+
+    int m = (l+r)/2;
+    if(v[m] < q){
+        return solve(v,q,m+1,r,v[m]);
     }
+    else return solve(v,q,l,m,ans);
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(false); cin.tie(0);
-    int n,m,q;
+int main(){
+    int n,m;
     cin >> n >> m;
     vector<int> v(n);
     for(int i =0;i<n;i++) cin >> v[i];
     for(int i =0;i<m;i++){
+        int q;
         cin >> q;
-        cout << solve(v,q,0,v.size()-1,-1) << "\n";
+        cout << solve(v,q,0,n-1,-1) << "\n";
     }
 }
