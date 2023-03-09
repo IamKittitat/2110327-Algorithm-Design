fe77304542feb418509c2ba0aa89ac184491cd1c
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-03-09 12:27:30 +0700

    a61_q1_thanos

--- grader/a61_q1_thanos/main.cpp
+++ grader/a61_q1_thanos/main.cpp
@@ -4,32 +4,26 @@
 
 using namespace std;
 
-long long solve(vector<int> &v,int a,int b,int l,int r){
-    auto it1 = lower_bound(v.begin(),v.end(),l);
-    auto it2 = upper_bound(v.begin(),v.end(),r);
-    int amount = it2-it1;
-    int len = r-l+1;
-    if(len == 1){
-        if(amount > 0) return b*amount;
-        else return a;
+long long solve(vector<int> &v,int A,int B,int l,int r){
+    int amount = upper_bound(v.begin(),v.end(),r) - lower_bound(v.begin(),v.end(),l);
+    if(l == r){
+        if(amount == 0) return A;
+        return B*amount;
     }
-    if(amount == 0) return a;
+    if(amount == 0) return A;
 
     int m = (l+r)/2;
-    long long halfPower = solve(v,a,b,l,m) + solve(v,a,b,m+1,r);
-    long long fullPower = 0;
-    if(amount > 0) fullPower = (long long)b*amount*len;
-    else fullPower = a;
+    long long half = solve(v,A,B,l,m) + solve(v,A,B,m+1,r);
+    long long total = amount > 0 ? (long long)B*amount*(r-l+1) : A;   
 
-    return min(halfPower,fullPower);
+    return min(total,half);
 }
 
 int main(){
-    int p,k,a,b;
-    cin >> p >> k >> a >> b;
-    int len = 1<<p;
+    int p,k,A,B;
+    cin >> p >> k >> A >> B;
     vector<int> v(k);
-    for(int i = 0;i<k;i++) cin >> v[i];
+    for(int i=0;i<k;i++) cin >> v[i];
     sort(v.begin(),v.end());
-    cout << solve(v,a,b,1,len);
+    cout << solve(v,A,B,1,1<<p);
 }
