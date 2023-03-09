a3985e1fc6a491f1a87b7b13eb4f732d4b61b9cc
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-03-09 12:13:50 +0700

    a62_q1a_recur

--- grader/a62_q1a_recur/main.cpp
+++ grader/a62_q1a_recur/main.cpp
@@ -14,16 +14,13 @@ void solve(vector<vector<int>> &v,int a,int b,int sx,int sy,int len){
     solve(v,a-1,b,sx+len/2,sy+len/2,len/2);
 }
 
-
-int main()
-{
-     ios_base::sync_with_stdio(false); cin.tie(0);
-     int a,b;
-     cin >> a >> b;
-     vector<vector<int>> v(1<<a,vector<int>(1<<a));
-     solve(v,a,b,0,0,1<<a);
-     for(auto &x : v){
+int main(){
+    int a,b;
+    cin >> a >> b;
+    vector<vector<int>> v(1<<a,vector<int>(1<<a,0));
+    solve(v,a,b,0,0,1<<a);
+    for(auto &x : v){
         for(auto &y : x) cout << y << " ";
-        cout << "\n";
-     }
+        cout << endl;
+    }
 }
