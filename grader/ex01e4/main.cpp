d6382df1d17fd5a1761e7225e51b54e2de77aa93
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-03-09 09:19:36 +0700

    ex01e4

--- grader/ex01e4/main.cpp
+++ grader/ex01e4/main.cpp
@@ -1,17 +1,16 @@
-#include<bits/stdc++.h>
+#include<iostream>
 
 using namespace std;
 
 int solve(int x,int n,int k){
     if(n == 1) return x%k;
-
-    int subProb = solve(x,n/2,k);
-    if(n%2 == 1) return ((subProb*subProb%k)*(x%k))%k;
-    else return subProb*subProb%k;
+    
+    int result = solve(x,n/2,k);
+    if(n%2 == 0) return (result*result)%k;
+    else return (((result*result)%k)*(x%k))%k;
 }
 
-int main()
-{
+int main(){
     int x,n,k;
     cin >> x >> n >> k;
     cout << solve(x,n,k);
