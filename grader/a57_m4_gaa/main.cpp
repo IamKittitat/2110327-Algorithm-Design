f69f3559a710056f2f1668da290bec1a3254a4f0
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-03-09 13:18:20 +0700

    a57_m4_gaa

--- grader/a57_m4_gaa/main.cpp
+++ grader/a57_m4_gaa/main.cpp
@@ -3,33 +3,26 @@
 
 using namespace std;
 
-string solve(int n,int k,vector<int> &v){
+string solve(int n,int len,int k){
     if(k == 0){
-        if(n == 0) return "g";
+        if(n == 1) return "g";
         return "a";
     }
 
-    if(n<v[k-1]) return solve(n,k-1,v);
-    if(n>=v[k-1]+k+3) return solve(n-(v[k-1]+k+3),k-1,v);
-
-    n-=v[k-1];
-    if(n==0) return "g";
+    int lm = (len - k - 2)/2 + 1,rm = lm + k + 2;
+    if(n < lm) return solve(n,lm-1,k-1);
+    if(n > rm) return solve(n-rm,lm-1,k-1);
+    if(n == lm) return "g";
     return "a";
 }
 
 int main(){
     int n;
     cin >> n;
-    vector<int> v;
-    v.push_back(3);
-    while(v.back() <= 1e9) v.push_back(v.back()*2+v.size()+3);
-    auto it = lower_bound(v.begin(),v.end(),n);
-    int k = it - v.begin();
-    // cout << k << endl;
-    cout << solve(n-1,k,v);
-    // for(int i =1;i<n;i++){
-    //     auto it = lower_bound(v.begin(),v.end(),i);
-    //     int k = it - v.begin();
-    //     cout << solve(i-1,k,v) << " ";
-    // }
+    int len = 3, k = 0;
+    while(len < n){
+        k++;
+        len = 2*len + k + 3;
+    }
+    cout << solve(n,len,k);
 }
