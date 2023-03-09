7e925eb99616eeb3dd40ef11d0717a920f3fdcee
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-03-09 13:54:21 +0700

    a60b_mid_p2_rope

--- grader/a60b_mid_p2_rope/main.cpp
+++ grader/a60b_mid_p2_rope/main.cpp
@@ -6,23 +6,17 @@ using namespace std;
 
 int main(){
     int n;
+    vector<int> v(3);
     cin >> n;
-    vector<int> v(3),dp(n+1);
     for(int i =0;i<3;i++) cin >> v[i];
     sort(v.begin(),v.end());
-    
-    // Not possible to be done
-    for(int i =1;i<v[0];i++){
+    vector<int> dp(n+1);
+    for(int i =1;i<=n;i++){
         dp[i] = INT32_MIN;
-    }
-    for(int i = v[0];i<=n;i++){
-        dp[i] = INT32_MIN;
-        for(int j =0;j<3;j++){
+        for(int j = 0;j<3;j++){
             if(v[j] > i) break;
-            dp[i] = max(dp[i-v[j]]+1,dp[i]);
+            dp[i] = max(dp[i],dp[i-v[j]]+1);
         }
     }
-    // for(auto &x : dp) cout << x << " ";
-    // cout << endl;
     cout << dp[n];
 }
