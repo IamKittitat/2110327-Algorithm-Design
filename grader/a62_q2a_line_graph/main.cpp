aaac3ff33b5b5b332ea4f8a9a0ca2343da82e425
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-04-02 14:52:14 +0700

    a62_q2a_line_graph

--- grader/a62_q2a_line_graph/main.cpp
+++ grader/a62_q2a_line_graph/main.cpp
@@ -3,47 +3,34 @@
 
 using namespace std;
 
-bool ok;
-int ans = 0;
-int one = 0;
-
-void dfs_i(vector<vector<int>> &edge,vector<int> &color,int v){
+bool dfs(vector<vector<int>> &g,vector<int> &color,vector<int> &parent,int v){
     color[v] = 1;
-    for(auto &u : edge[v]){
-        if(color[u] == 0) dfs_i(edge,color,u);
-        if(edge[u].size() == 1) one++;
-        if(edge[u].size() > 2) ok =false;
-        color[u] = 2;
-    }
-}
-
-void dfs(vector<vector<int>> &edge,vector<int> &color){
-    for(int i = 0;i<edge.size();i++){
-        ok = true;
-        one = 0;
-        if(color[i] == 0){
-            dfs_i(edge,color,i);
-            if(ok && (one == 2 || edge[i].size() == 0)) ans++;
+    if(g[v].size() > 2) return false;
+    for(auto &u : g[v]){
+        if(color[u] == 0){
+            parent[u] = v;
+            dfs(g,color,parent,u);
         }
+        if(color[u] == 1 && parent[v] != u) return false;
     }
+    color[v] = 2;
+    return true;
 }
 
 int main(){
-    int v,e;
-    cin >> v >> e;
-    vector<vector<int>> edge(v);
-    vector<int> color(v,0);
-    for(int i =0;i<e;i++){
+    int n,e;
+    cin >> n >> e;
+    vector<vector<int>> g(n);
+    for(int i = 0;i<e;i++){
         int a,b;
         cin >> a >> b;
-        edge[a].push_back(b);
-        edge[b].push_back(a);
+        g[a].push_back(b);
+        g[b].push_back(a);
     }
-    // for(int i = 0;i<v;i++){
-    //     cout << i << " : ";
-    //     for(auto &x : edge[i]) cout << x << " ";
-    //     cout << endl;
-    // }
-    dfs(edge,color);
+    int ans = 0;
+    vector<int> color(n,0),parent(n,-1);
+    for(int i = 0;i<n;i++){
+        if(color[i] == 0 && dfs(g,color,parent,i)) ans++;
+    }
     cout << ans;
 }
