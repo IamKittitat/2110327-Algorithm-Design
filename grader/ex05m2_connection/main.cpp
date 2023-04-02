74558747cc94db770e9fbc6d0aa38d021362f5b8
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-04-02 14:01:46 +0700

    ex05m2_connection

--- grader/ex05m2_connection/main.cpp
+++ grader/ex05m2_connection/main.cpp
@@ -4,45 +4,38 @@
 
 using namespace std;
 
-
-int ans = -1;
-
-void bfs(vector<vector<int>> &g,vector<int> &dist,int s){
-    queue<int> q;
-    dist[s] = 0;
-    q.push(s);
-    while(!q.empty()){
-        int v = q.front();
-        q.pop();
-        for(auto & u : g[v]){
-            if(dist[u] == INT32_MAX){
-                dist[u] = dist[v] + 1;
-                q.push(u);
-            }
-        }
-    }
-}
-
-int bfs_all(vector<vector<int>> &g,int k){
-    for(int i =0; i<g.size();i++){
-        vector<int> dist(g.size(),INT32_MAX);
-        bfs(g,dist,i);
-        int tmp = 0;
-        for(auto &d : dist) if(d <= k) tmp++;
-        ans = max(ans,tmp);
-    }
-    return ans;
-}
-
 int main(){
     int n,e,k;
     cin >> n >> e >> k;
     vector<vector<int>> g(n);
-    for(int i =0;i<e;i++){
+    for(int i = 0;i<e;i++){
         int a,b;
         cin >> a >> b;
         g[a].push_back(b);
         g[b].push_back(a);
     }
-    cout << bfs_all(g,k);
+
+    int ans = -1;
+    for(int i = 0;i<n;i++){
+        queue<int> q;
+        vector<int> dist(n,INT32_MAX);
+        q.push(i);
+        dist[i] = 0;
+        int tmp = 1;
+        while(!q.empty()){
+            int t = q.front();
+            q.pop();
+            for(auto &u : g[t]){
+                if(dist[u] == INT32_MAX){
+                    dist[u] = dist[t] + 1;
+                    if(dist[u] <= k){
+                        tmp++;
+                        q.push(u);
+                    }
+                }
+            }
+        }
+        ans = max(ans,tmp);
+    }
+    cout << ans;
 }
