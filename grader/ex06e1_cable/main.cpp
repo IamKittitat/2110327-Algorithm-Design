cc4bdfd2fc10d23371965ef9b0aabc648adcd724
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-04-02 15:48:40 +0700

    ex06e1_cable

--- grader/ex06e1_cable/main.cpp
+++ grader/ex06e1_cable/main.cpp
@@ -4,84 +4,95 @@
 
 using namespace std;
 
-typedef pair<int,pair<int,int>> kruskalPair;
+class DSU{
+    private:
+        vector<int> parent;
+    public: 
+        DSU(int n){
+            parent.resize(n);
+            for(int i = 0;i<n;i++) parent[i] = i;
+        }
+        int findRoot(int u){
+            if(parent[u] != u){
+                parent[u] = findRoot(parent[u]);
+            }
+            return parent[u];
+        }
+        int joinSet(int u,int v){
+            int pu = findRoot(u);
+            int pv = findRoot(v);
+            if(pu != pv){
+                parent[pu] = pv;
+                return true;
+            } else{
+                return false;
+            }
+        }
+};
 
-// PRIM ALGORITHM
-int prim(vector<vector<int>> w,int n){
+typedef pair<int,pair<int,int>> edge;
+
+int kruskal(priority_queue<edge,vector<edge>,greater<edge>> &e,int n){
+    DSU dsu(n);
+    int count = 0;
+    int ans = 0;
+    while(count < n-1){
+        edge t = e.top();
+        e.pop();
+        if(dsu.joinSet(t.second.first,t.second.second)){
+            ans+= t.first;
+            count++;
+        }
+    }
+    return ans;
+}   
+
+int findMin(vector<int> &dist){
+    int idx = INT32_MAX, minVal = INT32_MAX;
+    for(int i = 0;i<dist.size();i++){
+        if(dist[i] < minVal){
+            minVal = dist[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+
+int prim(vector<vector<int>> &g,int n){
+    int ans = 0;
     vector<int> dist(n,INT32_MAX);
     vector<bool> inMST(n,false);
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
     dist[0] = 0;
-    int ans = 0;
-    pq.push({dist[0],0});
-    while(!pq.empty()){
-        pair<int,int> p = pq.top();
-        pq.pop();
-        // cout << p.first << endl;
-        int v = p.second;
-        if(!inMST[v]){
-            inMST[v] = true;
-            ans += dist[v];
-            for(int i = 0;i<n;i++){
-                if(w[v][i] >= 0 && w[v][i] < dist[i] && !inMST[i]){
-                    dist[i] = w[v][i];
-                    pq.push({dist[i],i});
-                }
+    for(int i = 0;i<n;i++){
+        int u = findMin(dist);
+        ans += dist[u];
+        dist[u] = INT32_MAX;
+        inMST[u] = true;
+        for(int j = 0;j<n;j++){
+            if(!inMST[j] && g[u][j] < dist[j]){
+                dist[j] = g[u][j];
             }
         }
     }
     return ans;
 }
 
-// KRUSKAL ALGORITHM
-int findSet(vector<int> &dju, int n){
-    while(dju[n] != n) n = dju[n];
-    return n;
-}
-
-void unionSet(vector<int> &dju,int i, int j){
-    int u = findSet(dju,i), v = findSet(dju,j);
-    dju[u] = v;
-}
-
-int kruskal(vector<vector<int>> w,int n){
-    priority_queue<kruskalPair,vector<kruskalPair>,greater<kruskalPair>> pq;
-    vector<int> dju(n);
-    for(int i = 0;i<n;i++) dju[i] = i;
-    
-    for(int i = 0;i<n;i++){
-        for(int j = 0;j<n;j++) if(w[i][j] >= 0) pq.push({w[i][j],{i,j}});
-    }
-
-    int count = 0, ans = 0;
-    while(count < n-1){
-        kruskalPair p = pq.top();
-        int w = p.first, i = p.second.first, j = p.second.second;
-        pq.pop();
-        if(findSet(dju,i) != findSet(dju,j)){
-            count++;
-            ans += w;
-            unionSet(dju,i,j);
-        }
-    }
-    return ans;        
-}
-// ---------------------------------
-
 int main(){
     int n;
     cin >> n;
-    vector<vector<int>> w(n,vector<int>(n,-1));
+    vector<vector<int>> g(n,vector<int>(n,0));
+    priority_queue<edge,vector<edge>,greater<edge>> e;
     for(int i = 0;i<n-1;i++){
         for(int j = i+1;j<n;j++){
             int ip;
             cin >> ip;
-            w[i][j] = ip;
-            w[j][i] = ip;
+            g[i][j] = ip;
+            g[j][i] = ip;
+            e.push({ip,{i,j}});
         }
     }
-    // cout << prim(w,n);
-    cout << kruskal(w,n);
+    // cout << kruskal(e,n);
+    cout << prim(g,n);
 }
 
 /*
@@ -89,4 +100,4 @@ int main(){
 1 3 4
 2 7 
 6
-*/ 
+*/
