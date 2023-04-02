06f0192c7beb7a26b67b83ac9d9be3b081e18456
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-04-02 14:07:33 +0700

    a60a_q3_component

--- grader/a60a_q3_component/main.cpp
+++ grader/a60a_q3_component/main.cpp
@@ -11,28 +11,23 @@ void dfs(vector<vector<int>> &g,vector<int> &color,int v){
     color[v] = 2;
 }
 
-int dfs_all(vector<vector<int>> &g){
+int main(){
+    int n,e;
+    cin >> n >> e;
+    vector<vector<int>> g(n+1);
+    for(int i = 0;i<e;i++){
+        int a,b;
+        cin >> a >> b;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
     int ans = 0;
-    vector<int> color(g.size());
-    for(int i=0;i<g.size();i++){
+    vector<int> color(n+1,0);
+    for(int i = 1; i<=n;i++){
         if(color[i] == 0){
             ans++;
             dfs(g,color,i);
         }
     }
-    return ans;
-}
-
-int main(){
-    int v,e;
-    cin >> v >> e;
-    vector<vector<int>> g(v);
-    for(int i = 0;i<e;i++){
-        int a,b;
-        cin >> a >> b;
-        a--; b--;
-        g[a].push_back(b);
-        g[b].push_back(a);
-    }
-    cout << dfs_all(g);
+    cout << ans;
 }
