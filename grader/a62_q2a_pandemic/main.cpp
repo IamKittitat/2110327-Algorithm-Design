48742e7fb63f7ad745e3c22bde962609f8f4eb0e
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-04-02 14:22:36 +0700

    a62_q2a_pandemic

--- grader/a62_q2a_pandemic/main.cpp
+++ grader/a62_q2a_pandemic/main.cpp
@@ -7,32 +7,33 @@ using namespace std;
 int main(){
     int r,c,t;
     cin >> r >> c >> t;
-    vector<vector<int>> v(r,vector<int>(c));
-    for(int i = 0;i<r;i++){
-        for(int j = 0;j<c;j++){
-            cin >> v[i][j];
+    vector<vector<int>> g(r,vector<int>(c,0)),dist(r,vector<int>(c,INT32_MAX));
+    queue<pair<int,int>> q;
+    for(int i= 0 ;i<r;i++){
+        for(int j =0;j<c;j++){
+            cin >> g[i][j];
+            if(g[i][j] == 1) q.push({i,j});
         }
     }
-    
-    vector<pair<int,int>> direction = {{-1,0},{1,0},{0,1},{0,-1}};
-    queue<pair<int,int>> q;
-
-    // Loop t times
-    for(int i = 0;i<t;i++){
-        // Put every 1 to queue
-        for(int j = 0;j<r;j++) for(int k =0;k<c;k++) if(v[j][k] == 1) q.push({j,k});       
-        // Do 4 direction for every items in queue
+    vector<pair<int,int>> direction = {{0,1},{0,-1},{1,0},{-1,0}};
+    while(t--){
+        queue<pair<int,int>> tmp;
         while(!q.empty()){
-            pair<int,int> p = q.front(); q.pop();
+            pair<int,int> t = q.front();
+            q.pop();
             for(auto &d : direction){
-                int r1 = p.first + d.first, c1 = p.second + d.second;
-                if(r1 >= 0 && r1 < r && c1 >= 0 && c1 < c && v[r1][c1] != 2){
-                    v[r1][c1] = 1;
+                int i = t.first + d.first, j = t.second + d.second;
+                if(i >= 0 && i < r && j >= 0 && j < c && g[i][j] == 0){
+                    g[i][j] = 1;
+                    tmp.push({i,j});
                 }
             }
         }
+        q = tmp;
     }
+
     int ans = 0;
-    for(auto &x : v) for(auto &y : x) if(y == 1) ans++;
+    for(auto &x : g) for(auto &y : x)if(y == 1) ans++;
     cout << ans;
+
 }
