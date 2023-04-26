ffbbd2d271a90f2798d7889f65bb70cb9be5e00b
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-04-26 09:39:25 +0700

    ex04m1_sort

--- grader/ex04m1_sort/main.cpp
+++ grader/ex04m1_sort/main.cpp
@@ -8,6 +8,7 @@ int main(){
     int n;
     cin >> n;
     vector<int> v(n);
+    // Amount of one, two, three
     vector<int> amount(4,0);
     for(int i = 0; i< n ;i++ ){
         cin >> v[i];
@@ -16,6 +17,7 @@ int main(){
     int ans = 0,j = amount[1];
     for(int i = 0;i<amount[1];i++){
         if(v[i] == 2){
+            // Find next 1
             while(j<n && v[j] != 1) j++;
             ans++;
             swap(v[i],v[j]);
@@ -23,12 +25,15 @@ int main(){
     }
     for(int i = 0;i<amount[1];i++){
         if(v[i] == 3){
+            // Find next 1
             while(j<n && v[j] != 1) j++;
             ans++;
             swap(v[i],v[j]);
         }
     }
+    // If here : All 1 belong to their place + some of 2 belong to their place
 
+    // If found three in 2 place ==> Need swap
     for(int i = amount[1]; i<amount[1]+amount[2];i++){
         if(v[i] == 3){
            ans++;
