947d729b830502effd32ecdbbbcd320a7b63734c
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-04-30 15:33:33 +0700

    a64_q4_path_sum

--- grader/a64_q4_path_sum/main.cpp
+++ grader/a64_q4_path_sum/main.cpp
@@ -1,50 +1,45 @@
 #include<iostream>
 #include<vector>
-#include<map>
 
 using namespace std;
 
-int n,m, ub = 0;
+int n,m;
 vector<vector<pair<int,int>>> g;
-vector<int> ki(8);
 vector<int> maxIn;
 
-bool dfs(vector<int> &color,int v,int total,int needed,int ub){
-    if(total == needed) return true;
-    if(total > needed) return false;
-    if(total + ub < needed) return false;
-
+bool dfs(vector<int> &color,int v,int total,int need,int ub){
+    if(total > need) return false;
+    if(total == need) return true;
+    if(total + ub < need) return false;
     color[v] = 1;
     for(auto &u : g[v]){
         if(color[u.first] == 0){
             color[u.first] = 1;
-            if(dfs(color,u.first,total+u.second,needed,ub-maxIn[u.first])) return true;
+            if(dfs(color,u.first,total+u.second,need,ub-maxIn[u.first])) return true;
             color[u.first] = 0;
-        }   
+        }
     }
     color[v] = 0;
     return false;
 }
 
-void dfs_all(int needed){
+void dfs_all(int need,int ub){
     vector<int> color(n,0);
-    int total;
     for(int i = 0;i<n;i++){
-        if(color[i] == 0){
-            if(dfs(color,i,0,needed,ub-maxIn[i])){
-                cout << "YES\n";
-                return;
-            }
+        if(dfs(color,i,0,need,ub-maxIn[i])){
+            cout << "YES\n";
+            return;
         }
     }
     cout << "NO\n";
 }
 
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(0); 
+    ios_base::sync_with_stdio(false); cin.tie(0);
     cin >> n >> m;
     g.resize(n);
     maxIn.resize(n,-1);
+    vector<int> ki(8);
     for(int i = 0;i<8;i++) cin >> ki[i];
     for(int i = 0;i<m;i++){
         int a,b,w;
@@ -54,9 +49,9 @@ int main(){
         maxIn[a] = max(maxIn[a],w);
         maxIn[b] = max(maxIn[b],w);
     }
+    int ub = 0;
     for(auto &x : maxIn) ub += x;
-
     for(int i = 0;i<8;i++){
-        dfs_all(ki[i]);
+        dfs_all(ki[i],ub);
     }
 }
