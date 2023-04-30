3689609b41df066d8cfe87ad2e13c95845fd296a
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-04-30 12:55:18 +0700

    ex07e1_nqueen

--- grader/ex07e1_nqueen/main.cpp
+++ grader/ex07e1_nqueen/main.cpp
@@ -1,39 +1,87 @@
 #include<iostream>
 #include<vector>
+#include<stack>
 
 using namespace std;
 
-int ans = 0;
+int n, ans = 0;
 
-bool checkValid(vector<int> &sol,int i,int len){
-    for(int r1 = 0;r1<len;r1++){
-        // Check diagonal
-        if(abs(sol[r1]-i) == len-r1) return false;
+bool valid(vector<int> &sol,int len){
+    for(int i = 0;i<len;i++){
+        if(abs(sol[len]-sol[i]) == abs(len-i)) return false;
     }
     return true;
 }
 
-void solve(vector<int> &sol,vector<bool> &used,int len,int n){
+void solve(vector<int> &sol, vector<bool> &used, int len){
     if(len == n){
         ans++;
-        return;
+        return; 
     }
-    // Backtracking
     for(int i = 0;i<n;i++){
-        if(!used[i] && checkValid(sol,i,len)){
+        if(!used[i]){
             used[i] = true;
             sol[len] = i;
-            solve(sol,used,len+1,n);
+            if(valid(sol,len)) solve(sol,used,len+1);
             used[i] = false;
         }
     }
 }
 
 int main(){
-    int n;
     cin >> n;
     vector<int> sol(n,0);
     vector<bool> used(n,false);
-    solve(sol,used,0,n);
+    solve(sol,used,0);
     cout << ans;
 }
+
+/* using struct*/
+// int n, ans = 0;
+
+// struct state{
+//     vector<int> sol;
+//     int len;
+//     vector<bool> used;
+
+//     bool valid(){
+//         for(int i = 0;i<len;i++){
+//             if(abs(sol[i]-sol[len]) == abs(i-len)) return false;
+//         }
+//         return true;
+//     }
+// };
+
+
+// int main(){
+//     cin >> n;
+//     state init;
+//     vector<int> initSol(n,0);
+//     vector<bool> initUsed(n,false);
+//     init.sol = initSol;
+//     init.len = 0;
+//     init.used = initUsed;
+//     stack<state> s;
+//     s.push(init);
+//     while(!s.empty()){
+//         state t = s.top();
+//         s.pop();
+//         if(t.len == n) ans++;
+//         else{
+//             for(int i = 0;i<n;i++){
+//                 if(!t.used[i]){
+//                     state newT = t;
+//                     newT.used[i] = true;
+//                     newT.sol[t.len] = i;
+//                     if(newT.valid()){
+//                         newT.len++;
+//                         s.push(newT);
+//                     }
+//                     newT.used[i] = false;
+//                 }
+//             }
+//         }
+//     }
+//     cout << ans;
+    
+// }
