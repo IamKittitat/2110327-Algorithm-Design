2129445f59ffd5ebdfa4e69e5dbe6758f20d3105
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-04-30 18:12:57 +0700

    ex04e1_interval

--- grader/ex04e1_interval/main.cpp
+++ grader/ex04e1_interval/main.cpp
@@ -1,33 +1,25 @@
 #include<iostream>
-#include<queue>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
 int main(){
     int n;
     cin >> n;
-    vector<int> s(n),f(n);
-    vector<bool> selected(n,false);
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-    for(int i = 0; i<n;i++) cin >> s[i];
-    for(int i = 0; i < n;i++){
-        cin >> f[i];
-        pq.push({f[i],i});
-    }
-    
+    // pair<end,start>
+    vector<pair<int,int>> v(n);
+    for(int i = 0;i<n;i++) cin >> v[i].second;
+    for(int i = 0;i<n;i++) cin >> v[i].first;
+    sort(v.begin(),v.end());
+
     int ans = 0;
-    while(!pq.empty()){
-        pair<int,int> t = pq.top();
-        pq.pop();
-        int idx = t.second;
-        if(!selected[idx]){
-            // cout << idx << endl;
+    int lastFinish = -1;
+    for(int i = 0;i<n;i++){
+        if(v[i].second >= lastFinish){
+            lastFinish = v[i].first;
             ans++;
-            for(int i = 0;i<n;i++){
-                if(s[i] < f[idx]) selected[i] = true;
-            }
         }
     }
     cout << ans;
-    
 }
