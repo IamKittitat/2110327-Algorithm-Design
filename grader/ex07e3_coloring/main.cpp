0abdd72a50043109addb68dbff80f7a4961338c6
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-04-30 13:21:57 +0700

    ex07e3_coloring

--- grader/ex07e3_coloring/main.cpp
+++ grader/ex07e3_coloring/main.cpp
@@ -3,36 +3,41 @@
 
 using namespace std;
 
-bool solve(vector<vector<int>> &g,vector<int> &color,int maxColor,int idx,int n){
-    if(idx == n) return true;
-    
-    for(int i = 1;i<=maxColor;i++){
+int n,e;
+vector<vector<int>> g;
+vector<int> sol;
+
+bool solve(int v,int color){
+    if(v == n) return true;
+
+    for(int i = 0;i<color;i++){
         bool can = true;
-        for(auto &u : g[idx]){
-            if(u > idx) continue;
-            if (color[u] == i) can = false;
+        for(auto &u : g[v]){
+            if( u < v && sol[u] == i) can = false;
         }
         if(can){
-            color[idx] = i;
-            if(solve(g,color,maxColor,idx+1,n)) return true;
+            sol[v] = i;
+            if(solve(v+1,color)) return true;
         }
     }
     return false;
 }
 
 int main(){
-    int n,e;
     cin >> n >> e;
-    vector<vector<int>> g(n);
+    g.resize(n);
+
     for(int i = 0;i<e;i++){
         int a,b;
         cin >> a >> b;
         g[a].push_back(b);
         g[b].push_back(a);
     }
+
     for(int i = 1;i<=n;i++){
-        vector<int> color(n,0);
-        if(solve(g,color,i,0,n)){
+        sol.resize(n,-1);
+        sol[0] = 0;
+        if(solve(0,i)){
             cout << i;
             break;
         }
