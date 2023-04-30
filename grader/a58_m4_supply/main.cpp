8518639178e35eb685fa7521c25bc2141b13f972
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-04-30 17:58:22 +0700

    a58_m4_supply

--- grader/a58_m4_supply/main.cpp
+++ grader/a58_m4_supply/main.cpp
@@ -1,37 +1,41 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<algorithm>
 
 using namespace std;
 
-
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(0);
     int n,m,k;
     cin >> n >> m >> k;
-    vector<pair<int,int>> day(k+1);
-    queue<int> sent,request;
+    vector<pair<int,pair<int,int>>> v(k);
     for(int i = 0;i<k;i++){
         int d,e,l;
         cin >> d >> e >> l;
-        day[d] = {e,l};
+        v[i] = {d,{e,l}};
     }
-    for(int i = 1;i<=k;i++){
-        if(day[i].first == 0){
+    sort(v.begin(),v.end());
+    queue<int> request, produce;
+    for(int i = 0;i<k;i++){
+        int e,l;
+        e = v[i].second.first; l = v[i].second.second;
+
+        // e = 0 --> Produce | e = 1 --> Request
+        if(e == 0){
             if(request.empty()){
-                sent.push(day[i].second);
-                cout << 0 << "\n";
+                produce.push(l);
+                cout << "0\n";
             } else{
                 cout << request.front() << "\n";
                 request.pop();
             }
         } else{
-            if(sent.empty()){
-                request.push(day[i].second);
-                cout << 0 << "\n";
+            if(produce.empty()){
+                request.push(l);
+                cout << "0\n";
             } else{
-                cout << sent.front() << "\n";
-                sent.pop();
+                cout << produce.front() << "\n";
+                produce.pop();
             }
         }
     }
