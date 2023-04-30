70db735a808998615a3be3c72a2d5b68e4df8089
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-04-30 17:49:24 +0700

    a57_f3_drainage

--- grader/a57_f3_drainage/main.cpp
+++ grader/a57_f3_drainage/main.cpp
@@ -5,19 +5,21 @@
 using namespace std;
 
 int main(){
+    ios_base::sync_with_stdio(false); cin.tie(0);
     int n,l;
     cin >> n >> l;
-    vector<int> p(n);
-    for(int i = 0;i<n;i++) cin >> p[i];
-    sort(p.begin(),p.end());
-    int idx = p[0];
+    vector<int> v(n);
+    for(int i = 0;i<n;i++) cin >> v[i];
+    sort(v.begin(),v.end());
+    int idx = v[0];
     int ans = 1;
-    while(idx <= p[p.size()-1]){
+    while(idx < v.back()){
         idx += l;
-        auto it = lower_bound(p.begin(),p.end(),idx);
-        if(*it < idx) break;
+        auto it = lower_bound(v.begin(),v.end(),idx);
+        if(*it >= idx) idx = *it;
+        else break;
         ans++;
-        idx = *it;
     }
     cout << ans;
+
 }
